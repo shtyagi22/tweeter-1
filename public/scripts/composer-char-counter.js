3cb7f0c1008f9d1815d54f7ed292986f7ef06add
shtyagi22 <sh.tyagi22@gmail.com>
2022-09-17 00:12:10 -0400

    implemennted the tweet words counter with the help of jquery and event listeners

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,20 @@
-$(document).ready(function() {
-  // --- our code goes here ---
+$(document).ready(function () {
+  $(".tweet-text").on('input', onInput);
 });
+
+const onInput = function (event) {
+  let $input = $(this);
+  let len = $input.val().length;
+  let charsLeft = 140 - len;
+
+  const $form = $input.closest('form');
+  const $counter = $form.find('span.counter');
+
+  $counter.text(charsLeft);
+
+  if (charsLeft < 0) {
+    $('.counter').addClass('form--red');
+  } else {
+    $('.counter').removeClass('form--red');
+  }
+}
